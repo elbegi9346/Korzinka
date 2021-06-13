6fde6b0740802f9bb3b0516cc69ece5085a6b54f
elbegi9346 <elbegi9346@yandex.ru>
2021-06-13 10:50:46 +0300

    project korzinka in HTML, CSS and JS

---
+++ js/script.js
@@ -0,0 +1,49 @@
+window.addEventListener('DOMContentLoaded', () =>{
+
+  let products = document.querySelectorAll('.product'),
+    buttons = document.querySelectorAll('button'),
+    openBtn = document.querySelector('.open');
+
+  function creatCart() {
+    let cart = document.createElement('div'),
+        field = document.createElement('div'),
+        heading = document.createElement('h2'),
+        closeBtn = document.createElement('button');
+
+  cart.classList.add('cart');
+  field.classList.add('cart-field');
+  closeBtn.classList.add('close');
+
+  heading.textContent = 'В нашей корзине';
+  closeBtn.textContent = 'Закрить';
+
+  document.body.appendChild(cart);
+  cart.appendChild(heading)
+  cart.appendChild(field)
+  cart.appendChild(closeBtn)
+  }
+  creatCart();
+
+  let cart = document.querySelector('.cart'),
+      closeBtn = document.querySelector('.close'),
+      field = document.querySelector('.cart-field');
+
+  openBtn.addEventListener('click', () =>{
+    cart.style.display = 'block';
+  });
+
+  closeBtn.addEventListener('click', () =>{
+    cart.style.display = 'none';
+  });
+
+  buttons.forEach(function (item, i) {
+    item.addEventListener('click', () =>{
+      let cloneItem = products[i].cloneNode(true),
+          btn = cloneItem.querySelector('button');
+
+      btn.remove();
+      field.appendChild(cloneItem);
+      products[i].remove();
+    });
+  });
+});
